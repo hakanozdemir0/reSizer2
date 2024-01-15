be1e7f16bd49bc03c53e8374a0beb7745938dae5
hakan özdemir <hakanozdemr1@gmail.com>
2024-01-15 22:02:27 +0300

    main restore

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from PIL import Image
 
 foto =Image.open("foto.jpg")
 
+#fotograf için verilmek istenilen boyutlar
+
+
 eni = int(input("Eni: "))
 boyu = int(input("Boyu: "))
 
@@ -11,6 +14,10 @@ newsize = (eni, boyu)
 foto1 = foto.resize(newsize)
 
 foto1.show()
+
+
+#fotografı bulundugunuz klasore kaydetmeye yarar
+
 
 yeni_foto_adi = "yenifoto.jpg"
 foto1.save(yeni_foto_adi)
